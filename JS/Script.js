5c6d19f72c2c20d32e89e46a6e4d8337a5a237be
Aland Wasim Rasul <alandwasimrasul@gmail.com>
2025-02-10 20:29:31 +0300

    Change file index & Js

--- JS/Script.js
+++ JS/Script.js
@@ -1,213 +1,213 @@
 const letterData = {
     'A': {
-images: [
-    { src: 'imagee/Alphabet_Aa/Apple.png', label: 'Apple' },
-    { src: 'imagee/Alphabet_Aa/Ant.jpg', label: 'Ant' },
-    { src: 'imagee/Alphabet_Aa/Amblance2.png', label: 'Ambulance' },
-    { src: 'imagee/Alphabet_Aa/Alligator.png', label: 'Alligator' }
-]
-},
-'B': {
-images: [
-    { src: 'imagee/Alphabet_Bb/Bag4.png', label: 'Bag' },
-    { src: 'imagee/Alphabet_Bb/Book.png', label: 'Book' },
-    { src: 'imagee/Alphabet_Bb/Bee3.png', label: 'Bee' },
-    { src: 'imagee/Alphabet_Bb/Boy.png', label: 'Boy' }
-]
-},
-'C': {
-images: [
-    { src: 'imagee/Alphabet_Cc/Cat.png', label: 'Cat' },
-    { src: 'imagee/Alphabet_Cc/Car.png', label: 'Car' },
-    { src: 'imagee/Alphabet_Cc/Cup.png', label: 'Cup' },
-    { src: 'imagee/Alphabet_Cc/Cake2.png', label: 'Cake' }
-]
-},
-'D': {
-images: [
-    { src: 'imagee/Alphabet_Dd/Duck.png', label: 'Duck' },
-    { src: 'imagee/Alphabet_Dd/Dog.png', label: 'Dog' },
-    { src: 'imagee/Alphabet_Dd/Doll.png', label: 'Doll' },
-    { src: 'imagee/Alphabet_Dd/Door.png', label: 'Door' }
-]
-},
-'E': {
-images: [
-    { src: 'imagee/Alphabet_Ee/Egg.png', label: 'Egg' },
-    { src: 'imagee/Alphabet_Ee/Elephant.png', label: 'Elephant' },
-    { src: 'imagee/Alphabet_Ee/Elk.png', label: 'Elk' },
-    { src: 'imagee/Alphabet_Ee/Earth.png', label: 'Earth' }
-    
-]
-},
-'F': {
-images: [
-    { src: 'imagee/Alphabet_Ff/Fig.png', label: 'Fig' },
-    { src: 'imagee/Alphabet_Ff/Frog.png', label: 'Frog' },
-    { src: 'imagee/Alphabet_Ff/Fox.png', label: 'Fox' },
-    { src: 'imagee/Alphabet_Ff/Flower.png', label: 'Flower' }
-]
-},
-'G': {
-images: [
-    { src: 'imagee/Alphabet_Gg/Girl.png', label: 'Girl' },
-    { src: 'imagee/Alphabet_Gg/Goat.png', label: 'Goat' },
-    { src: 'imagee/Alphabet_Gg/Game.png', label: 'Game' },
-    { src: 'imagee/Alphabet_Gg/Gift.png', label: 'Gift' }
-]
-},
-'H': {
-images: [
-    { src: 'imagee/Alphabet_Hh/Hat.png', label: 'Hat' },
-    { src: 'imagee/Alphabet_Hh/Hen.png', label: 'Hen' },
-    { src: 'imagee/Alphabet_Hh/Horse.png', label: 'Horse' },
-     { src: 'imagee/Alphabet_Hh/House.png', label: 'House' }
-]
-},
-'I': {
-images: [
-    { src: 'imagee/Alphabet_Ii/Ice.png', label: 'Ice' },
-    { src: 'imagee/Alphabet_Ii/Ice_cream.png', label: 'Ice cream' },
-    { src: 'imagee/Alphabet_Ii/Island.png', label: 'Island' },
-    { src: 'imagee/Alphabet_Ii/Igloo.png', label: 'Igloo' }
-]
-},
-'J': {
-images: [
-    { src: 'imagee/Alphabet_Jj/Juice.png', label: 'Juice' },
-    { src: 'imagee/Alphabet_Jj/Jam.png', label: 'Jam' },
-    { src: 'imagee/Alphabet_Jj/Jeep.png', label: 'Jeep' },
-    { src: 'imagee/Alphabet_Jj/Jug.png', label: 'Jug' }
-]
-},
-'K': {
-images: [
-    { src: 'imagee/Alphabet_Kk/King.png', label: 'King' },
-    { src: 'imagee/Alphabet_Kk/Key.png', label: 'Key' },
-    { src: 'imagee/Alphabet_Kk/Kangaroo.png', label: 'Kangaroo' },
-    { src: 'imagee/Alphabet_Kk/Kite.png', label: 'Kite' }
-]
-},
-'L': {
-images: [
-     { src: 'imagee/Alphabet_Ll/Lion.png', label: 'Lion' },
-    { src: 'imagee/Alphabet_Ll/Leaf.png', label: 'Leaf' },
-     { src: 'imagee/Alphabet_Ll/Lamp.png', label: 'Lamp' },
-    { src: 'imagee/Alphabet_Ll/Leg.png', label: 'Leg' }
-]
-},
-'M': {
-images: [
-    { src: 'imagee/Alphabet_Mm/Monkey.png', label: 'Monkey' },
-    { src: 'imagee/Alphabet_Mm/Moon.png', label: 'Moon' },
-    { src: 'imagee/Alphabet_Mm/Mouse.png', label: 'Mouse' },
-     { src: 'imagee/Alphabet_Mm/Milk.png', label: 'Milk' }
-]
-},
-'N': {
-images: [
-    { src: 'imagee/Alphabet_Nn/Night.png', label: 'Night' },
-    { src: 'imagee/Alphabet_Nn/Nose.png', label: 'Nose' },
-    { src: 'imagee/Alphabet_Nn/Nest.png', label: 'Nest' },
-    { src: 'imagee/Alphabet_Nn/Nurse.png', label: 'Nurse' }
-]
-},
-'O': {
-images: [
-    { src: 'imagee/Alphabet_Oo/Orange.png', label: 'Orange' },
-    { src: 'imagee/Alphabet_Oo/Owl.png', label: 'Owl' },
-     { src: 'imagee/Alphabet_Oo/Orca.png', label: 'Orca' },
-    { src: 'imagee/Alphabet_Oo/Ox.png', label: 'Ox' }
-]
-},
-'P': {
-images: [
-    { src: 'imagee/Alphabet_Pp/Pen.png', label: 'Pen' },
-    { src: 'imagee/Alphabet_Pp/Planet2.png', label: 'Planet' },
-     { src: 'imagee/Alphabet_Pp/Potato.png', label: 'Potato' },
-    { src: 'imagee/Alphabet_Pp/Parrot.png', label: 'Parrot' }
-]
-},
-'Q': {
-images: [
-     { src: 'imagee/Alphabet_Qq/Queen.png', label: 'Queen' },
-    { src: 'imagee/Alphabet_Qq/Quilt.png', label: 'Quilt' },
-    { src: 'imagee/Alphabet_Qq/Quiz.png', label: 'Quiz' },
-     { src: 'imagee/Alphabet_Qq/quill.png', label: 'quill' }
-]
-},
-'R': {
-images: [
-    { src: 'imagee/Alphabet_Rr/Rabbit.png', label: 'Rabbit' },
-    { src: 'imagee/Alphabet_Rr/Rain.png', label: 'Rain' },
-     { src: 'imagee/Alphabet_Rr/Red.png', label: 'Red' },
-    { src: 'imagee/Alphabet_Rr/Ring.png', label: 'Ring' }
-]
-},
-'S': {
-images: [
-    { src: 'imagee/Alphabet_Ss/Sun.png', label: 'Sun' },
-    { src: 'imagee/Alphabet_Ss/Star.png', label: 'Star' },
-     { src: 'imagee/Alphabet_Ss/Snake.png', label: 'Snake' },
-     { src: 'imagee/Alphabet_Ss/snowman.png', label: 'snowman' }
-]
-},
-'T': {
-images: [
-    { src: 'imagee/Alphabet_Tt/Tree.png', label: 'Tree' },
-     { src: 'imagee/Alphabet_Tt/Tomato.png', label: 'Tomato' },
-    { src: 'imagee/Alphabet_Tt/Turtle.png', label: 'Turtle' },
-     { src: 'imagee/Alphabet_Tt/Tiger.png', label: 'Tiger' }
-]
-},
-'U': {
-images: [
-     { src: 'imagee/Alphabet_Uu/Umbrella.png', label: 'Umbrella' },
-    { src: 'imagee/Alphabet_Uu/UFO.png', label: 'UFO' },
-    { src: 'imagee/Alphabet_Uu/Uncle.png', label: 'Uncle' },
-     { src: 'imagee/Alphabet_Uu/Unicorn.png', label: 'unicorn' }
-]
-},
-'V': {
-images: [
-    { src: 'imagee/Alphabet_Vv/Van.png', label: 'Van' },
-     { src: 'imagee/Alphabet_Vv/Vase.png', label: 'Vase' },
-     { src: 'imagee/Alphabet_Vv/Vest.png', label: 'Vest' },
-    { src: 'imagee/Alphabet_Vv/Violin.png', label: 'Violin' }
-]
-},
-'W': {
-images: [
-    { src: 'imagee/Alphabet_Ww/Water.png', label: 'Water' },
-    { src: 'imagee/Alphabet_Ww/Wall.png', label: 'Wall' },
-     { src: 'imagee/Alphabet_Ww/Watch.png', label: 'Watch' },
-    { src: 'imagee/Alphabet_Ww/Worm.png', label: 'Worm' }
-]
-},
-'X': {
-images: [
-   { src: 'imagee/Alphabet_Xx/X-ray.png', label: 'X-ray' },
-   { src: 'imagee/Alphabet_Xx/Xylophone.png', label: 'Xylophone' },
-   { src: 'imagee/Alphabet_Xx/Xerox.png', label: 'Xerox' },
-   { src: 'imagee/Alphabet_Xx/Xmas.png', label: 'Xmas' }
-]
-},
-'Y': {
-images: [
-    { src: 'imagee/Alphabet_Yy/Yellow.png', label: 'Yellow' },
-    { src: 'imagee/Alphabet_Yy/Yarn.png', label: 'Yarn' },
-     { src: 'imagee/Alphabet_Yy/Yak.png', label: 'Yak' },
-    { src: 'imagee/Alphabet_Yy/Yogurt.png', label: 'Yogurt' }
-]
-},
-'Z': {
-images: [
-   { src: 'imagee/Alphabet_Zz/Zebra.png', label: 'Zebra' },
-   { src: 'imagee/Alphabet_Zz/Zoo.png', label: 'Zoo' },
-    { src: 'imagee/Alphabet_Zz/Zip.png', label: 'Zip' },
-   { src: 'imagee/Alphabet_Zz/Zero.png', label: 'Zero' }
-]
-}
+        images: [
+            { src: '/imagee/Alphabet_Aa/Apple.png', label: 'Apple' },
+            { src: '/imagee/Alphabet_Aa/Ant.jpg', label: 'Ant' },
+            { src: '/imagee/Alphabet_Aa/Amblance2.png', label: 'Ambulance' },
+            { src: '/imagee/Alphabet_Aa/Alligator.png', label: 'Alligator' }
+        ]
+    },
+    'B': {
+        images: [
+            { src: '/imagee/Alphabet_Bb/Bag4.png', label: 'Bag' },
+            { src: '/imagee/Alphabet_Bb/Book.png', label: 'Book' },
+            { src: '/imagee/Alphabet_Bb/Bee3.png', label: 'Bee' },
+            { src: '/imagee/Alphabet_Bb/Boy.png', label: 'Boy' }
+        ]
+    },
+    'C': {
+        images: [
+            { src: '/imagee/Alphabet_Cc/Cat.png', label: 'Cat' },
+            { src: '/imagee/Alphabet_Cc/Car.png', label: 'Car' },
+            { src: '/imagee/Alphabet_Cc/Cup.png', label: 'Cup' },
+            { src: '/imagee/Alphabet_Cc/Cake2.png', label: 'Cake' }
+        ]
+    },
+    'D': {
+        images: [
+            { src: '/imagee/Alphabet_Dd/Duck.png', label: 'Duck' },
+            { src: '/imagee/Alphabet_Dd/Dog.png', label: 'Dog' },
+            { src: '/imagee/Alphabet_Dd/Doll.png', label: 'Doll' },
+            { src: '/imagee/Alphabet_Dd/Door.png', label: 'Door' }
+        ]
+    },
+    'E': {
+        images: [
+            { src: '/imagee/Alphabet_Ee/Egg.png', label: 'Egg' },
+            { src: '/imagee/Alphabet_Ee/Elephant.png', label: 'Elephant' },
+            { src: '/imagee/Alphabet_Ee/Elk.png', label: 'Elk' },
+            { src: '/imagee/Alphabet_Ee/Earth.png', label: 'Earth' }
+
+        ]
+    },
+    'F': {
+        images: [
+            { src: '/imagee/Alphabet_Ff/Fig.png', label: 'Fig' },
+            { src: '/imagee/Alphabet_Ff/Frog.png', label: 'Frog' },
+            { src: '/imagee/Alphabet_Ff/Fox.png', label: 'Fox' },
+            { src: '/imagee/Alphabet_Ff/Flower.png', label: 'Flower' }
+        ]
+    },
+    'G': {
+        images: [
+            { src: '/imagee/Alphabet_Gg/Girl.png', label: 'Girl' },
+            { src: '/imagee/Alphabet_Gg/Goat.png', label: 'Goat' },
+            { src: '/imagee/Alphabet_Gg/Game.png', label: 'Game' },
+            { src: '/imagee/Alphabet_Gg/Gift.png', label: 'Gift' }
+        ]
+    },
+    'H': {
+        images: [
+            { src: '/imagee/Alphabet_Hh/Hat.png', label: 'Hat' },
+            { src: '/imagee/Alphabet_Hh/Hen.png', label: 'Hen' },
+            { src: '/imagee/Alphabet_Hh/Horse.png', label: 'Horse' },
+            { src: '/imagee/Alphabet_Hh/House.png', label: 'House' }
+        ]
+    },
+    'I': {
+        images: [
+            { src: '/imagee/Alphabet_Ii/Ice.png', label: 'Ice' },
+            { src: '/imagee/Alphabet_Ii/Ice_cream.png', label: 'Ice cream' },
+            { src: '/imagee/Alphabet_Ii/Island.png', label: 'Island' },
+            { src: '/imagee/Alphabet_Ii/Igloo.png', label: 'Igloo' }
+        ]
+    },
+    'J': {
+        images: [
+            { src: '/imagee/Alphabet_Jj/Juice.png', label: 'Juice' },
+            { src: '/imagee/Alphabet_Jj/Jam.png', label: 'Jam' },
+            { src: '/imagee/Alphabet_Jj/Jeep.png', label: 'Jeep' },
+            { src: '/imagee/Alphabet_Jj/Jug.png', label: 'Jug' }
+        ]
+    },
+    'K': {
+        images: [
+            { src: '/imagee/Alphabet_Kk/King.png', label: 'King' },
+            { src: '/imagee/Alphabet_Kk/Key.png', label: 'Key' },
+            { src: '/imagee/Alphabet_Kk/Kangaroo.png', label: 'Kangaroo' },
+            { src: '/imagee/Alphabet_Kk/Kite.png', label: 'Kite' }
+        ]
+    },
+    'L': {
+        images: [
+            { src: '/imagee/Alphabet_Ll/Lion.png', label: 'Lion' },
+            { src: '/imagee/Alphabet_Ll/Leaf.png', label: 'Leaf' },
+            { src: '/imagee/Alphabet_Ll/Lamp.png', label: 'Lamp' },
+            { src: '/imagee/Alphabet_Ll/Leg.png', label: 'Leg' }
+        ]
+    },
+    'M': {
+        images: [
+            { src: '/imagee/Alphabet_Mm/Monkey.png', label: 'Monkey' },
+            { src: '/imagee/Alphabet_Mm/Moon.png', label: 'Moon' },
+            { src: '/imagee/Alphabet_Mm/Mouse.png', label: 'Mouse' },
+            { src: '/imagee/Alphabet_Mm/Milk.png', label: 'Milk' }
+        ]
+    },
+    'N': {
+        images: [
+            { src: '/imagee/Alphabet_Nn/Night.png', label: 'Night' },
+            { src: '/imagee/Alphabet_Nn/Nose.png', label: 'Nose' },
+            { src: '/imagee/Alphabet_Nn/Nest.png', label: 'Nest' },
+            { src: '/imagee/Alphabet_Nn/Nurse.png', label: 'Nurse' }
+        ]
+    },
+    'O': {
+        images: [
+            { src: '/imagee/Alphabet_Oo/Orange.png', label: 'Orange' },
+            { src: '/imagee/Alphabet_Oo/Owl.png', label: 'Owl' },
+            { src: '/imagee/Alphabet_Oo/Orca.png', label: 'Orca' },
+            { src: '/imagee/Alphabet_Oo/Ox.png', label: 'Ox' }
+        ]
+    },
+    'P': {
+        images: [
+            { src: '/imagee/Alphabet_Pp/Pen.png', label: 'Pen' },
+            { src: '/imagee/Alphabet_Pp/Planet2.png', label: 'Planet' },
+            { src: '/imagee/Alphabet_Pp/Potato.png', label: 'Potato' },
+            { src: '/imagee/Alphabet_Pp/Parrot.png', label: 'Parrot' }
+        ]
+    },
+    'Q': {
+        images: [
+            { src: '/imagee/Alphabet_Qq/Queen.png', label: 'Queen' },
+            { src: '/imagee/Alphabet_Qq/Quilt.png', label: 'Quilt' },
+            { src: '/imagee/Alphabet_Qq/Quiz.png', label: 'Quiz' },
+            { src: '/imagee/Alphabet_Qq/quill.png', label: 'quill' }
+        ]
+    },
+    'R': {
+        images: [
+            { src: '/imagee/Alphabet_Rr/Rabbit.png', label: 'Rabbit' },
+            { src: '/imagee/Alphabet_Rr/Rain.png', label: 'Rain' },
+            { src: '/imagee/Alphabet_Rr/Red.png', label: 'Red' },
+            { src: '/imagee/Alphabet_Rr/Ring.png', label: 'Ring' }
+        ]
+    },
+    'S': {
+        images: [
+            { src: '/imagee/Alphabet_Ss/Sun.png', label: 'Sun' },
+            { src: '/imagee/Alphabet_Ss/Star.png', label: 'Star' },
+            { src: '/imagee/Alphabet_Ss/Snake.png', label: 'Snake' },
+            { src: '/imagee/Alphabet_Ss/snowman.png', label: 'snowman' }
+        ]
+    },
+    'T': {
+        images: [
+            { src: '/imagee/Alphabet_Tt/Tree.png', label: 'Tree' },
+            { src: '/imagee/Alphabet_Tt/Tomato.png', label: 'Tomato' },
+            { src: '/imagee/Alphabet_Tt/Turtle.png', label: 'Turtle' },
+            { src: '/imagee/Alphabet_Tt/Tiger.png', label: 'Tiger' }
+        ]
+    },
+    'U': {
+        images: [
+            { src: '/imagee/Alphabet_Uu/Umbrella.png', label: 'Umbrella' },
+            { src: '/imagee/Alphabet_Uu/UFO.png', label: 'UFO' },
+            { src: '/imagee/Alphabet_Uu/Uncle.png', label: 'Uncle' },
+            { src: '/imagee/Alphabet_Uu/Unicorn.png', label: 'unicorn' }
+        ]
+    },
+    'V': {
+        images: [
+            { src: '/imagee/Alphabet_Vv/Van.png', label: 'Van' },
+            { src: '/imagee/Alphabet_Vv/Vase.png', label: 'Vase' },
+            { src: '/imagee/Alphabet_Vv/Vest.png', label: 'Vest' },
+            { src: '/imagee/Alphabet_Vv/Violin.png', label: 'Violin' }
+        ]
+    },
+    'W': {
+        images: [
+            { src: '/imagee/Alphabet_Ww/Water.png', label: 'Water' },
+            { src: '/imagee/Alphabet_Ww/Wall.png', label: 'Wall' },
+            { src: '/imagee/Alphabet_Ww/Watch.png', label: 'Watch' },
+            { src: '/imagee/Alphabet_Ww/Worm.png', label: 'Worm' }
+        ]
+    },
+    'X': {
+        images: [
+            { src: '/imagee/Alphabet_Xx/X-ray.png', label: 'X-ray' },
+            { src: '/imagee/Alphabet_Xx/Xylophone.png', label: 'Xylophone' },
+            { src: '/imagee/Alphabet_Xx/Xerox.png', label: 'Xerox' },
+            { src: '/imagee/Alphabet_Xx/Xmas.png', label: 'Xmas' }
+        ]
+    },
+    'Y': {
+        images: [
+            { src: '/imagee/Alphabet_Yy/Yellow.png', label: 'Yellow' },
+            { src: '/imagee/Alphabet_Yy/Yarn.png', label: 'Yarn' },
+            { src: '/imagee/Alphabet_Yy/Yak.png', label: 'Yak' },
+            { src: '/imagee/Alphabet_Yy/Yogurt.png', label: 'Yogurt' }
+        ]
+    },
+    'Z': {
+        images: [
+            { src: '/imagee/Alphabet_Zz/Zebra.png', label: 'Zebra' },
+            { src: '/imagee/Alphabet_Zz/Zoo.png', label: 'Zoo' },
+            { src: '/imagee/Alphabet_Zz/Zip.png', label: 'Zip' },
+            { src: '/imagee/Alphabet_Zz/Zero.png', label: 'Zero' }
+        ]
+    }
     // Add more letters as needed
 };
 
@@ -219,21 +219,21 @@ let touchStartTime = 0;
 function speak(text) {
     window.speechSynthesis.cancel();
     const utterance = new SpeechSynthesisUtterance(text);
-    
-    let femaleVoice = voices.find(voice => 
-        voice.name.includes('Zira') || 
+
+    let femaleVoice = voices.find(voice =>
+        voice.name.includes('Zira') ||
         voice.name.includes('Samantha') ||
         (voice.name.toLowerCase().includes('female') && voice.lang.includes('en-US'))
     );
-    
+
     if (!femaleVoice) {
         femaleVoice = voices.find(voice => voice.lang.includes('en-US'));
     }
-    
+
     if (femaleVoice) {
         utterance.voice = femaleVoice;
     }
-    
+
     utterance.pitch = 1.2;
     utterance.rate = 0.9;
     window.speechSynthesis.speak(utterance);
